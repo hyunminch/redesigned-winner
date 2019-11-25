022e24446c8c90edb15cf4e61f61123046c9979f
Hyun Min Choi <hyunmin.personal@gmail.com>
2019-11-25 22:38:09 +0900

    Implement video sharing api

--- videosharex/videofeed/models.py
+++ videosharex/videofeed/models.py
@@ -6,7 +6,11 @@ class YouTubeVideoRecommendation(models.Model):
     poster = models.ForeignKey(User, on_delete=models.CASCADE)
     video = models.CharField(max_length=16)
     text = models.CharField(max_length=120, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
 class YouTubeVideoRecommendationShare(models.Model):
     sharer = models.ForeignKey(User, on_delete=models.CASCADE)
     recommendation = models.ForeignKey(YouTubeVideoRecommendation, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
--- videosharex/videofeed/views.py
+++ videosharex/videofeed/views.py
@@ -6,7 +6,7 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework import status
 
-from videofeed.models import YouTubeVideoRecommendation
+from videofeed.models import YouTubeVideoRecommendation, YouTubeVideoRecommendationShare
 from videofeed.serializers import YouTubeVideoRecommendationSerializer, YouTubeVideoRecommendationShareSerializer
 
 
@@ -34,14 +34,32 @@ class RecommendYouTubeVideo(APIView):
     permission_classes = [IsAuthenticated]
     
     def get(self, request, format=None):
-        recommendations = YouTubeVideoRecommendation.objects.order_by('-id')
+        _following = request.user.following.all()
+        following = [f.followed for f in _following]
+
+        shares = YouTubeVideoRecommendationShare.objects.order_by('-created_at')
+
+        following_shares = shares
+        # following_shares = [share for share in shares if share.sharer in following]
+
+        recommendations = [share.recommendation for share in following_shares]
         serializer = YouTubeVideoRecommendationSerializer(recommendations, many=True)
         return Response(serializer.data)
 
     def post(self, request, format=None):
         request.data['poster'] = request.user.id
         serializer = YouTubeVideoRecommendationSerializer(data=request.data)
+
         if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            recommendation = serializer.save()
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+        share = YouTubeVideoRecommendationShare()
+        share.sharer = request.user
+        share.recommendation = recommendation 
+        share.save()
+
+        serializer = YouTubeVideoRecommendationShareSerializer(share)
+
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
