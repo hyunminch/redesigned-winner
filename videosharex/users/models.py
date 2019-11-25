9f1175ade4312d99de49c3547f5b2e112667a8ac
Hyun Min Choi <hyunmin.personal@gmail.com>
2019-11-25 19:53:56 +0900

    Update views and models

--- videosharex/users/models.py
+++ videosharex/users/models.py
@@ -1,2 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User, Group
 
+
+class Follower(models.Model):
+    follower = models.ForeignKey(User, on_delete=models.CASCADE, related_name='following')
+    followed = models.ForeignKey(User, on_delete=models.CASCADE, related_name='followers') 
