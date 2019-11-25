461cbf46e0842f1e5f65b9fae4fd6942b78449a8
Hyun Min Choi <hyunmin.personal@gmail.com>
2019-11-25 19:53:56 +0900

    Update views and models

--- videosharex/users/serializers.py
+++ videosharex/users/serializers.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import serializers
 
+from users.models import Follower
+
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
@@ -11,3 +13,18 @@ class GroupSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Group
         fields = ['url', 'name']
+
+# TODO(hyunminch): implement
+class FollowerSerializer(serializers.ModelSerializer):
+    follower = serializers.PrimaryKeyRelatedField(queryset=User.objects.all())
+    followed = serializers.PrimaryKeyRelatedField(queryset=User.objects.all())
+
+    def get_follower(self, obj):
+        return obj.follower.username
+
+    def get_followed(self, obj):
+        return obj.followed.username
+
+    class Meta:
+        fields = '__all__'
+        model = Follower 
--- videosharex/users/views.py
+++ videosharex/users/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User, Group
 from rest_framework import viewsets
+from rest_framework.views import APIView
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework import status
 
-from users.serializers import UserSerializer, GroupSerializer
+from users.serializers import UserSerializer, GroupSerializer, FollowerSerializer
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -12,3 +17,15 @@ class UserViewSet(viewsets.ModelViewSet):
 class GroupViewSet(viewsets.ModelViewSet):
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
+
+class Follow(APIView):
+    authentication_classes = [SessionAuthentication, BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+    
+    def post(self, request, format=None):
+        request.data['follower'] = request.user.id
+        serializer = FollowerSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
