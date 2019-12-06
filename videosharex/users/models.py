56a15c4f7148a5ffd8d87e8f9872b6da2851b312
Hyun Min Choi <hyunmin.personal@gmail.com>
2019-12-06 16:57:32 +0900

    Implement user apis

--- videosharex/users/models.py
+++ videosharex/users/models.py
@@ -2,6 +2,17 @@ from django.db import models
 from django.contrib.auth.models import User, Group
 
 
+class ProfileManager(models.Manager):
+    def create_profile(self, user, profile):
+        profile = self.create(user=user, profile=profile)
+        return profile
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile = models.CharField(max_length=500, blank=True)
+
+    objects = ProfileManager()
+
 class Follower(models.Model):
     follower = models.ForeignKey(User, on_delete=models.CASCADE, related_name='following')
     followed = models.ForeignKey(User, on_delete=models.CASCADE, related_name='followers') 
